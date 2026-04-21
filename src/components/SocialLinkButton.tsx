e601f78b0777e59530f3ed1686a60cf95fd201b9
MOG\itspa <itspandorux@gmail.com>
2026-04-21 20:37:56 +1000

    Made Basic Button Component

---
+++ src/components/SocialLinkButton.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+interface Props {
+  text: string;
+  link: string;
+  color: string;
+}
+
+function SocialLinkButton({ text, link, color }: Props) {
+  return (
+    <button
+      type="button"
+      className={"btn btn-" + color}
+      onClick={() => window.open(link, "_blank", "noopener, noreferrer")}
+    >
+      {text}
+    </button>
+  );
+}
+
+export default SocialLinkButton;
